f6a94ad6c9c51f3b361eac2216eccc7f915a2469
Nitish Kumar <79355266+MrNitishroy@users.noreply.github.com>
2024-05-26 01:04:10 +0530

    Hover Effect added

--- lib/Pages/HomePage/HomePage.dart
+++ lib/Pages/HomePage/HomePage.dart
@@ -1,4 +1,7 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:portfolio/Widgets/HoverWidget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,27 +9,18 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Column(
-        children: [
-          Text(
-            "Headline Medium",
-            style: Theme.of(context).textTheme.headlineMedium,
-          ),
-          Text(
-            "Body Large",
-            style: Theme.of(context).textTheme.bodyLarge,
-          ),
-          Text(
-            "Body medium",
-            style: Theme.of(context).textTheme.bodyMedium,
-          ),
-          Text(
-            "Lable medium",
-            style: Theme.of(context).textTheme.labelMedium,
-          ),
-        ],
+      body: Center(
+        child: HoverEffect(
+          builder: (value) {
+            return Text(
+              "This is Text ",
+              style: TextStyle(
+                color: value ? Colors.pink : Colors.yellow,
+              ),
+            );
+          },
+        ),
       ),
-    ));
+    );
   }
 }
